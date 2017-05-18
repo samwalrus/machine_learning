d05054cb1ccfcf60fec0b9890038d344fbb4bf28
Sam Neaves <sam.neaves@gmail.com>
2017-05-18 08:16:58 +0100

    Feature construction working, need to add attribute names to features.

--- classifiers/rule_featureconstruction.pl
+++ classifiers/rule_featureconstruction.pl
@@ -1,4 +1,4 @@
-%:-use_module(library('~/media/sam/9bb6ab40-5f17-481e-aba8-7bd9e4e05d66/home/sam/Documents/Prolog_practise/logical_purity.pl')).
+:-use_module('/media/sam/9bb6ab40-5f17-481e-aba8-7bd9e4e05d66/home/sam/Documents/Prolog_practise/logical_purity.pl').
 
 
 %This file makes binary features from attributes that can take a number of values.
@@ -29,7 +29,7 @@ attributes_data_features(As,D,AName-Pairs):-
 	maplist(i_i_p,Fs,Truths,Pairs).
 
 i_i_p(I1,I2,I1-I2).
-    
+
 attributelist_features(As,Fs):-
 	setof(X, member(X,As),Fs).
 
@@ -58,7 +58,7 @@ run(Fs,FeatureValuesPairs,FeatureNames,Rows,New):-
 	findall(Row,multimember(Row,FeatureValues),Rows),
 	New =[FeatureNames|Rows].
 
-%This gives a demo of how the code works 	
+%This gives a demo of how the code works
 %?- run(Fs,FvPs,FNs,Rows,New), forall(member(M,New),format("~w~n",[M])).
 
 
@@ -101,27 +101,36 @@ test(Features):-
 	maplist(features_values_truths(Features),As2,Truths),
 	writeln(Truths).
 
-thinga(As):-
+collumn_class(As-Classes):-
 	data2([Headers|Data]),
 	fronts_lasts_lists(DataNoClass,Classes,Data),
 	multimember(As,DataNoClass). %choice point here
 
-thingb(As2):-
-	findall(As,thinga(As),As2).
+all_collumns(As2):-
+	findall(As,collumn_class(As),As2).
 
-thingc(As,Features,Truths):-
+thingc(As,Classes,Features,Truths):-
 	maplist(make_int,As,As2),
 	length(DataNoClass,L),
 	numlist(1,L,Numlist),
 	maplist(idnumber_class_attributevalue_struct,Numlist,Classes,As2,Is),
 	gen_feature(Is,Features),
 	maplist(features_values_truths(Features),As2,Truths).
-
-thingd(Features,Truths):-
+%I need to put the attribute names in the features
+thingd(Features,Truths,New):-
 	thingb(As2),
-	maplist(thingc,As2,Features,Truths).
-	
-	
+	maplist(i_i_pair,As,Classes,As2),
+	maplist(thingc,As,Classes,Features,Truths),
+	lists_concatlist(Features,FsCat),
+	findall(Zs,multimember(Zs,Truths),NewZs),
+	maplist(lists_concatlist,NewZs,Concated),
+	lists_concatlist([[FsCat],Concated],New).
+
+
+
+
+
+i_i_pair(I,I2,I-I2).
 
 idnumber_class_attributevalue_struct(I,C,A,i(I,C,A)).
 
@@ -141,7 +150,7 @@ fronts_lasts_lists(Fronts,Lasts,Lists):-
 %@ Headers = [a1, a2, class],
 %@ Data = [[5, 6, pos], [7, 8, neg], [3, 5, pos], [7, 2, pos], [4, 4, pos], [4, 6, neg]],
 %@ Attributes = [[5, 6], [7, 8], [3, 5], [7, 2], [4, 4], [4, 6]],
-%@ Classes = [pos, neg, pos, pos, pos, neg] 
+%@ Classes = [pos, neg, pos, pos, pos, neg]
 %@ Unknown action:  (h for help)
 				%@ Action? .
 
@@ -163,7 +172,7 @@ data2(D):-
 value_feature_truth(Value,R-Threshold,T):-
 	Thing =.. [R,Value,Threshold,T],
 	call(Thing).
-	
+
 features_values_truths(Fs,V,Ts):-
 	maplist(value_feature_truth(V),Fs,Ts).
 
